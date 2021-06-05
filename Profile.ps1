591150ff093746ce867db4b9ba16ce2328c318d3
James Rayman <rayman.james.e@gmail.com>
2021-06-05 03:17:52 -0500

    update windows terminal and powershell

--- Profile.ps1
+++ Profile.ps1
@@ -1,14 +1,3 @@
-Set-PSReadlineOption -Color @{
-    "Command" = [ConsoleColor]::Yellow
-    "Parameter" = [ConsoleColor]::Gray
-    "Operator" = [ConsoleColor]::Magenta
-    "Variable" = [ConsoleColor]::Green
-    "String" = [ConsoleColor]::White
-    "Number" = [ConsoleColor]::Blue
-    "Type" = [ConsoleColor]::Cyan
-    "Comment" = [ConsoleColor]::Blue
-}
-
 function prompt {
     $currentDirectory = $(Get-Location)
 
@@ -19,3 +8,4 @@ function prompt {
 
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
